faecbbc187fba519cd21285a98f7a9fef2852892
Liv Galendez <livcg1@gmail.com>
2015-07-01 14:39:15 +1000

    Shortened row labels

--- public/js/matrix.js
+++ public/js/matrix.js
@@ -4,15 +4,18 @@ $(document).ready(function() {
     //*** ...
 
     // Dynamically set column labels based on row labels
-    var val;
-    for (i = 1; i <= 5; i++) {
-	val = $("input.label.g5i" + i).attr("value");
-	$("td.label.g5i" + i).html(val);
+    for (groupIndex = 1; groupIndex <= 5; groupIndex++) {
+    
+	var val;
+	for (itemIndex = 1; itemIndex <= 5; itemIndex++) {
+	    val = $("input.label.g" + groupIndex + "i" + itemIndex).attr("value");
+	    $("td.label.g" + groupIndex + "i" + itemIndex).html(val);
 
-	$("input.label.g5i" + i).change( function() {
-	    var val = $(this).val()
-	    $("td.label." + this.classList[0]).html(val); // TODO: Check for relevant class
-	});	
+	    $("input.label.g" + groupIndex + "i" + itemIndex).change( function() {
+		var val = $(this).val()
+		$("td.label." + this.classList[0]).html(val); // TODO: Check for relevant class
+	    });
+	}
     }
 
     // Toggle cells between 'X', 'O', 'X?', 'O?', and ''
