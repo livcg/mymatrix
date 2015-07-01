cdf14fb113431ee46d8ecbbc1906d6c6ab7a6d75
Liv Galendez <livcg1@gmail.com>
2015-07-01 14:58:13 +1000

    Dynamically set column group labels based on row group labels

--- public/js/matrix.js
+++ public/js/matrix.js
@@ -1,15 +1,23 @@
 
 $(document).ready(function() {
-    // Dynamically set column group labels based on row group labels
-    //*** ...
-
-    // Dynamically set column labels based on row labels
-    for (groupIndex = 1; groupIndex <= 5; groupIndex++) {
-    
+    for (groupIndex = 2; groupIndex <= 5; groupIndex++) {
 	var val;
+
+	// Dynamically set column group labels based on row group labels
+
+	val = $("input.label.group" + groupIndex).val()
+	$("td.label.group" + groupIndex).text(val);
+
+	$("input.label.group" + groupIndex).change( function() {
+	    var val = $(this).val()
+	    $("td.label." + this.classList[0]).text(val); // TODO: Check for relevant class
+	});
+
+	// Dynamically set column labels based on row labels
+	
 	for (itemIndex = 1; itemIndex <= 5; itemIndex++) {
-	    val = $("input.label.g" + groupIndex + "i" + itemIndex).attr("value");
-	    $("td.label.g" + groupIndex + "i" + itemIndex).html(val);
+	    val = $("input.label.g" + groupIndex + "i" + itemIndex).val()
+	    $("td.label.g" + groupIndex + "i" + itemIndex).html(val); //*** note text()?
 
 	    $("input.label.g" + groupIndex + "i" + itemIndex).change( function() {
 		var val = $(this).val()
